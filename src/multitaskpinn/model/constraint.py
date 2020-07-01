924e2fc995ebdc627fdf9d5196921ca7ce87dd1c
Gert-Jan <gertjanboth@gmail.cmo>
2020-07-01 15:40:52 +0200

    Added deepymod code

---
+++ src/multitaskpinn/model/constraint.py
@@ -0,0 +1,29 @@
+'''This module contains the '''
+
+
+import torch
+from deepymod_torch import Constraint
+
+
+class LeastSquares(Constraint):
+    ''' Calculates coefficients of constraint via least squares
+        using QR decomposition. 
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def calculate_coeffs(self, sparse_thetas, time_derivs):
+        """[summary]
+
+        Args:
+            sparse_thetas (List[]): [description]
+            time_derivs ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        opt_coeff = []
+        for theta, dt in zip(sparse_thetas, time_derivs):
+            Q, R = torch.qr(theta)  # solution of lst. sq. by QR decomp.
+            opt_coeff.append(torch.inverse(R) @ Q.T @ dt)
+        return opt_coeff
